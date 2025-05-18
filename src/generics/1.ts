499b90e8a631927e9919e189fe89bbfa5e511f28
Vladyslav6 <vlad51284@gmail.com>
2025-05-18 23:42:34 +0300

    add5TaskGen

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get<T>(url);
--- src/generics/5.ts
+++ src/generics/5.ts
@@ -1,12 +1,13 @@
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
+type UserRoleType = Record<UserRole, string>;
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+const RoleDescription: UserRoleType = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
 };
