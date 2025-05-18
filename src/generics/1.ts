20c0e8b0b40caad95479f2737c99eb36ada6156c
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-05-18 17:21:09 +0300

    add4task

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-async function fetchData(url) {
+
+
+
+async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url); // Вказуємо axios, який тип даних очікуємо
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
